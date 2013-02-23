6ad866e0848ab790af7ab457f7cd3a27139841e1
Chris Goosen <goosenc@gmail.com>
2013-02-23 12:38:06 +0200

    Added comment to control layer class

--- nerd-rage/src/com/nerdrage/layers/ControlLayer.java
+++ nerd-rage/src/com/nerdrage/layers/ControlLayer.java
@@ -1,5 +1,10 @@
 package com.nerdrage.layers;
 
+/**
+ * This layer will be used to display the control overlay. It will accept user input and
+ * relay these the user's key presses back to another layer. This layer can be switched to
+ * redirect the control layer's commands to another layer. 
+ */
 public class ControlLayer {
 
 }
