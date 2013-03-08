ffaf16dc1782d44b6c65362b441cbaa1cedf9da0
Chris Goosen <goosenc@gmail.com>
2013-03-08 18:16:35 +0200

    Changed back to displaying the splash first

--- nerd-rage/src/com/nerdrage/NerdRageGame.java
+++ nerd-rage/src/com/nerdrage/NerdRageGame.java
@@ -18,7 +18,7 @@ public class NerdRageGame extends Game {
 	@Override
 	public void create() {		
 		fpsLogger = new FPSLogger();
-		setScreen(new GameScreen());
+		setScreen(new SplashScreen(this));
 	}
 
 	@Override
